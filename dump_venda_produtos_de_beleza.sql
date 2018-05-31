7e1fe60b1b02885ee5502bd7dd7fdad91fedc8a1
João Marcos <joao@joaomarcoss.com>
2018-05-31 14:10:47 -0300

    inserts produtos de beleza

---
+++ dump_venda_produtos_de_beleza.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: auladosudo.mysql.dbaas.com.br    Database: auladosudo
+-- ------------------------------------------------------
+-- Server version	5.6.35-81.0-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `CadastroProdutoBeleza`
+--
+
+DROP TABLE IF EXISTS `CadastroProdutoBeleza`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `CadastroProdutoBeleza` (
+  `cod` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` char(50) COLLATE latin1_general_ci NOT NULL,
+  `filial` char(50) COLLATE latin1_general_ci NOT NULL,
+  `preco` double NOT NULL,
+  `validade` date NOT NULL,
+  `foto` blob NOT NULL,
+  PRIMARY KEY (`cod`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `CadastroProdutoBeleza`
+--
+
+LOCK TABLES `CadastroProdutoBeleza` WRITE;
+/*!40000 ALTER TABLE `CadastroProdutoBeleza` DISABLE KEYS */;
+INSERT INTO `CadastroProdutoBeleza` VALUES (1,'Batom','Avon',12.12,'2019-10-10',''),(2,'Perfume','Boticario',23.23,'2019-10-10','');
+/*!40000 ALTER TABLE `CadastroProdutoBeleza` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `VendaProdutoBeleza`
+--
+
+DROP TABLE IF EXISTS `VendaProdutoBeleza`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `VendaProdutoBeleza` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `CadastroProdutoBeleza_cod` int(10) unsigned NOT NULL,
+  `produto_id` int(10) unsigned DEFAULT NULL,
+  `usuario_id` int(10) unsigned DEFAULT NULL,
+  `valor` float DEFAULT NULL,
+  `forma_pagamento` int(10) unsigned DEFAULT NULL,
+  `parcelas` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `VendaProdutoBeleza_FKIndex1` (`CadastroProdutoBeleza_cod`),
+  CONSTRAINT `VendaProdutoBeleza_ibfk_1` FOREIGN KEY (`CadastroProdutoBeleza_cod`) REFERENCES `CadastroProdutoBeleza` (`cod`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `VendaProdutoBeleza`
+--
+
+LOCK TABLES `VendaProdutoBeleza` WRITE;
+/*!40000 ALTER TABLE `VendaProdutoBeleza` DISABLE KEYS */;
+INSERT INTO `VendaProdutoBeleza` VALUES (1,2,NULL,NULL,23.23,0,1),(2,1,NULL,NULL,12.12,1,1),(3,1,NULL,NULL,12.12,1,1),(4,1,NULL,NULL,12.12,1,1),(5,1,NULL,NULL,12.12,1,1),(6,2,NULL,NULL,23.23,1,1),(7,1,NULL,NULL,12.12,1,1),(8,1,NULL,NULL,12.12,1,1),(9,1,NULL,NULL,12.12,1,1),(10,1,NULL,NULL,12.12,1,1),(11,2,NULL,NULL,23.23,1,1),(12,1,NULL,NULL,12.12,1,1),(13,1,NULL,NULL,12.12,1,1),(14,1,NULL,NULL,12.12,1,1),(15,1,NULL,NULL,12.12,1,1),(16,1,NULL,NULL,12.12,1,1);
+/*!40000 ALTER TABLE `VendaProdutoBeleza` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-31 14:06:04
